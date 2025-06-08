37c96def99c43a2c93d36f9cfb7d179c6918eff8
Tamerlan Imanov <tamerlan.imanov@develops.today>
2025-06-08 20:45:10 +0400

    fixed the shares

--- handlers/post/sharePost.go
+++ handlers/post/sharePost.go
@@ -1,13 +1,15 @@
 package post
 
 import (
+	"net/http"
+	"strconv"
+	"time"
+
 	db "github.com/genryusaishigikuni/ch4nge/database"
 	"github.com/genryusaishigikuni/ch4nge/models"
 	"github.com/genryusaishigikuni/ch4nge/utils"
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
-	"net/http"
-	"strconv"
 )
 
 func SharePost(c *gin.Context) {
@@ -27,19 +29,26 @@ func SharePost(c *gin.Context) {
 
 	// Create share
 	share := models.PostShare{
-		PostID: utils.ParseUint(postID),
-		UserID: uint(userID),
+		PostID:   utils.ParseUint(postID),
+		UserID:   uint(userID),
+		SharedAt: time.Now(),
 	}
 	if err := db.DB.Create(&share).Error; err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to share post"})
 		return
 	}
 
-	// Update share count
-	db.DB.Model(&models.Post{}).Where("id = ?", postID).Update("shares_number", gorm.Expr("shares_number + 1"))
+	// Increment share count
+	db.DB.Model(&models.Post{}).
+		Where("id = ?", postID).
+		Update("shares_number", gorm.Expr("shares_number + 1"))
 
 	// Return updated post
 	var post models.Post
-	db.DB.First(&post, postID)
+	if err := db.DB.First(&post, postID).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch post"})
+		return
+	}
+
 	c.JSON(http.StatusOK, post)
 }
